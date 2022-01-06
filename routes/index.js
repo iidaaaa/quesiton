7bbd2eb4000f320cb1277626f3bc5654c017a5f1
iidaaaa <iida.oshima@gmail.com>
2022-01-07 02:15:29 +0900

    0107

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,6 @@ var data = new Date()
 router.get('/', function(req, res) {
 
     const query = req.query.p;
-    const files = glob.sync("public/media/*");
     const shuffle = ([...array]) => {
         for (let i = array.length - 1; i >= 0; i--) {
           const j = Math.floor(Math.random() * (i + 1));
@@ -17,8 +16,17 @@ router.get('/', function(req, res) {
         }
         return array;
     }
-
+    // 
+    file_random = glob.sync("public/media/*");
+    var shuffle_file_random = shuffle(file_random)
+    const files = glob.sync(shuffle_file_random[0] + '/*')
     var shuffle_files = shuffle(files);
+    console.log(shuffle_files)
+    //
+    // const files = glob.sync("public/media/*");
+    
+    // var shuffle_files = shuffle(files);
+    // console.log(shuffle_files)
     
 
     res.render('index.ejs', {
@@ -31,23 +39,24 @@ router.get('/', function(req, res) {
 
 router.post('/', function(req, res, next){
 
-    var newData = req.body
-    console.log(newData);
-    var Month = (data.getMonth()+1).toString();
-    var Date = (data.getDate()).toString();
-    var Hour = (data.getHours()).toString();
-    var Min = (data.getMinutes()).toString();
-    var Sec = (data.getSeconds()).toString();
+  var newData = req.body
+  console.log(newData);
+  var Month = (data.getMonth()+1).toString();
+  var Date = (data.getDate()).toString();
+  var Hour = (data.getHours() + 9).toString();
+  var Min = (data.getMinutes()).toString();
+  var Sec = (data.getSeconds()).toString();
 
 
+  var write_data = JSON.stringify(newData);
+  console.log(write_data);
+
+  fs.appendFileSync('test.txt', write_data + '\n');
+  console.log('test.txtに追記されました');
 
 
-
-
-    var write_data = JSON.stringify(newData);
-    console.log(write_data);
-    fs.writeFileSync(Month + "月" + Date + "日" + Hour + ":" + Min + ":" + Sec + '_out.txt', write_data);
-    res.render('thank.ejs');
+  //fs.writeFileSync(Month + "月" + Date + "日" + Hour + ":" + Min + ":" + Sec + '_out.txt', write_data);
+  res.render('thank.ejs');
 
 })
 
